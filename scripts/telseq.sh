f5d5e5cbedaeec34e979d42a8517f13c45e554fb
Rose Runyan <roserunyan@.gmail.com>
2026-03-26 10:57:58 -0400

    telseq.sh for briggsae

--- scripts/telseq.sh
+++ scripts/telseq.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
-#SBATCH --job-name=20260325_telseq_elegans
+#SBATCH --job-name=20260326_telseq_briggsae
 #SBATCH --partition=parallel
 #SBATCH --account=eande106
-#SBATCH --time=08:00:00
+#SBATCH --time=06:00:00
 #SBATCH --mail-user=rrunyan1@jh.edu
 #SBATCH --mail-type=ALL
 #SBATCH --cpus-per-task=2
-#SBATCH --array=1-14
+#SBATCH --array=1-15
 #SBATCH --output=/home/rrunyan1/andersen_lab/output/%x/%A_%a.out
 
-################################################
-### estimate telomere lengths for C. elegans ###
-################################################
+#################################################
+### estimate telomere lengths for C. briggsae ###
+#################################################
 
 # load required modules
 module load gcc BamTools/2.5.2
 
+# change based on species script is being run on
+SPECIES="briggsae" # options: elegans, briggsae, tropicalis
 
 # select each bamlist file as an array job
-BAMLIST_LIST="/home/rrunyan1/Rose/isotype_reference_strains_lists/bamlist_lists/elegans_bamlist_list.txt" # list of each sub bamlist
+BAMLIST_LIST="/home/rrunyan1/Rose/isotype_reference_strains_lists/bamlist_lists/${SPECIES}_bamlist_list.txt" # list of each sub bamlist
 # print current line of bamlist list file and delete the rest to perform script on that line (bamlist) only, repeat for each line of file to do script for each bamlist
 CURRENT_BAMLIST=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $BAMLIST_LIST) # each job in the array will be one sub bamlist
 
 # set paths
 TELSEQ="/home/rrunyan1/data-eande106/software/telseq/telseq/src/Telseq/telseq" # path to telseq software
-BAMLIST="/home/rrunyan1/Rose/isotype_reference_strains_lists/elegans_bamlist_split/${CURRENT_BAMLIST}" # path to sub bamlist the job is using
-OUT_DIR="/home/rrunyan1/Rose/results/telseq/elegans" # path to output directory
+BAMLIST="/home/rrunyan1/Rose/isotype_reference_strains_lists/split_bamlists/${SPECIES}/${CURRENT_BAMLIST}" # path to sub bamlist the job is using
+OUT_DIR="/home/rrunyan1/Rose/results/telseq/${SPECIES}" # path to output directory
 mkdir -p ${OUT_DIR} # make output directory if it doesn't exist already
 
 # run telseq

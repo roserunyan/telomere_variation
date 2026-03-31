5e4e21aff6170df5eb5bb624e832a41245343463
Rose Runyan <roserunyan@.gmail.com>
2026-03-31 16:25:20 -0400

    update nemascan.sh

--- scripts/nemascan.sh
+++ scripts/nemascan.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-module load python/anaconda
+# Run this script in tmux session
 source activate /data/eande106/software/conda_envs/nf24_env
 
-SPECIES="elegans"
+# Only change this variable and nothing else to either elegans, briggsae, or tropicalis
+SPECIES="briggsae"
 
+# change to working directory where .nextflow.log will be output
+cd /home/rrunyan1/Rose/analysis/nemascan/${SPECIES}
+
+# choose VCF file to use based on species
+if [[ ${SPECIES} == "elegans" ]]; then
+    VCF="20250625"
+elif [[ ${SPECIES} == "briggsae" ]]; then
+    VCF="20250626"
+elif [[ ${SPECIES} == "tropicalis" ]]; then
+    VCF="20250627"
+fi
+
+# run NemaScan
 nextflow run -latest andersenlab/nemascan \
---vcf 20250625 \
+--vcf ${VCF} \
 --traitfile /home/rrunyan1/Rose/results/telseq/${SPECIES}/${SPECIES}_telo_lengths.tsv \
 --species c_${SPECIES} \
 --out /home/rrunyan1/Rose/results/nemascan/${SPECIES}
-#Choose between c_elegans, c_tropicalis or c_briggsae
-
-#nextflow run -latest andersenlab/nemascan -profile mappings --vcf 20210121 --traitfile input_data/c_elegans/phenotypes/PC1.tsv

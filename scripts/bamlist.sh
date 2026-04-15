2ba868621fa42bf5d5d33d11a8b1eafb3174e61a
Rose Runyan <roserunyan@.gmail.com>
2026-04-15 14:26:02 -0400

    update bamlist.sh with high power panel

--- scripts/bamlist.sh
+++ scripts/bamlist.sh
@@ -5,3 +5,7 @@
 awk '{print "/home/rrunyan1/vast/data/c_elegans/WI/alignments/"$0".bam"}' elegans_strains.txt > elegans_bams.txt
 awk '{print "/home/rrunyan1/vast/data/c_briggsae/WI/alignments/"$0".bam"}' briggsae_strains.txt > briggsae_bams.txt
 awk '{print "/home/rrunyan1/vast/data/c_tropicalis/WI/alignments/"$0".bam"}' tropicalis_strains.txt > tropicalis_bams.txt
+
+# Make bamlist for each bam in the highest power mapping panel
+cd /home/rrunyan1/Rose/isotype_reference_strains_lists/high_power_mapping_panels
+awk '{print "/home/rrunyan1/vast/data/c_elegans/WI/alignments/"$0".bam"}' ce_allout15_strains.txt > /home/rrunyan1/Rose/isotype_reference_strains_lists/bamlists/elegans_allout15_bams.txt
